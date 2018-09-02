075096d454c4ca94db21a9e1fc4cf4d7e4fc8335
ErikaSkaar <Erika.ballering@my.tccd.edu>
2018-09-01 19:19:23 -0500

    built basic menu--need to get display menu and menu options registeed

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -10,7 +10,52 @@ namespace PizzaCreator
     {
         static void Main(string[] args)
         {
+            DisplayMenu();
 
+            bool unlessQuit;
+
+            do
+            {
+                unlessQuit = DisplayMenu();
+            } while (unlessQuit);
         }
+    }
+}
+
+private static bool DisplayMenu()
+{
+    while (true)
+    {
+        Console.WriteLine("N)ewOrder");
+        Console.WriteLine("M)odifyOrder");
+        Console.WriteLine("D)isplayorder");
+        Console.WriteLine("Q)uit");
+
+        string input = Console.ReadLine();
+
+        switch (input[0])
+        {
+            case 'N': NewOrder(); break;
+            case 'M': ModifyOrder(); break;
+            case 'D': DisplayOrder(); break;
+            case 'Q':; break;
+            default:
+                Console.WriteLine("Please enter a valid value"); break;
+                switch (input[0])
+                {
+                    case 'n':
+                    case 'N': NewOrder(); return true;
+
+                    case 'm':
+                    case 'M': ModifyOrder(); return true;
+
+                    case 'd':
+                    case 'D': DispalyMenu(); return true;
+
+                    case 'q':
+                    case 'Q': return false;
+                    default: Console.WriteLine("Please enter a valid value"); break; //break; = breaking out of the switch statment
+                };
+        };
     }
 }
