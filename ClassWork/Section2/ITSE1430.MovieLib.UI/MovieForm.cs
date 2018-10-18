3f18d25abb734cf1680f0f13cf166bc2c937a29f
erika.ballering <erika.ballering@my.tccd.edu>
2018-10-17 19:23:11 -0500

    interfaces and enumerables

--- ClassWork/Section2/ITSE1430.MovieLib.UI/MovieForm.cs
+++ ClassWork/Section2/ITSE1430.MovieLib.UI/MovieForm.cs
@@ -12,14 +12,33 @@ namespace ITSE1430.MovieLib.UI
 {
     public partial class MovieForm : Form
     {
+        #region Construction
+
         public MovieForm()
         {
             InitializeComponent();
         }
+        #endregion
 
         public Movie Movie { get; set; }
 
-        #region Event Handlers 
+        private void MovieForm_Load( object sender, EventArgs e )
+        {
+            //_btnSave.Click += _btnSave_Click;
+            if (Movie != null)
+            {
+                _txtName.Text = Movie.Name;
+                _txtDescription.Text = Movie.Description;
+                _txtReleaseYear.Text = Movie.ReleaseYear.ToString();
+                _txtRunLength.Text = Movie.RunLength.ToString();
+                _chkOwned.Checked = Movie.IsOwned;
+            };
+
+            ValidateChildren();
+        }
+
+        #region Event Handlers
+
         private void OnCancel( object sender, EventArgs e )
         {
             DialogResult = DialogResult.Cancel;
@@ -31,6 +50,14 @@ namespace ITSE1430.MovieLib.UI
             if (!ValidateChildren())
                 return;
 
+            //var movie = new Movie();                        
+            //movie.Name = _txtName.Text;
+            //movie.Description = _txtDescription.Text;            
+            //movie.ReleaseYear = GetInt32(_txtReleaseYear);            
+            //movie.RunLength = GetInt32(_txtRunLength);            
+            //movie.IsOwned = _chkOwned.Checked;
+
+            //Initializer syntax
             var movie = new Movie()
             {
                 Name = _txtName.Text,
@@ -44,48 +71,23 @@ namespace ITSE1430.MovieLib.UI
             DialogResult = DialogResult.OK;
             Close();
         }
-        #endregion
 
-        private int GetInt32( TextBox textBox )
-        {
-            if (String.IsNullOrEmpty(textBox.Text))
-                return -1;
-
-            if (Int32.TryParse(textBox.Text, out var value))
-                return value;
-            return -1;
-        }
-
-        private void MovieForm_Load( object sender, EventArgs e )
-        {
-            if (Movie != null)
-            {
-                _txtName.Text = Movie.Name;
-                _txtDescription.Text = Movie.Description;
-                _txtReleaseYear.Text = Movie.ReleaseYear.ToString();
-                _txtRunLength.Text = Movie.RunLength.ToString();
-                _chkOwned.Checked = Movie.IsOwned;
-            };
-
-            ValidateChildren();
-        }
-
-        private void OnValidateName( object sender, CancelEventArgs e )
+        private void OnValidateName( object sender, System.ComponentModel.CancelEventArgs e )
         {
             var control = sender as TextBox;
 
             if (String.IsNullOrEmpty(control.Text))
             {
+                //control.Error
                 _errors.SetError(control, "Name is required");
                 e.Cancel = true;
             } else
                 _errors.SetError(control, "");
         }
 
-        private void OnValidatingReleaseYear( object sender, CancelEventArgs e )
+        private void OnValidatingReleaseYear( object sender, System.ComponentModel.CancelEventArgs e )
         {
             var control = sender as TextBox;
-
             var result = GetInt32(control);
             if (result < 1900)
             {
@@ -95,16 +97,31 @@ namespace ITSE1430.MovieLib.UI
                 _errors.SetError(control, "");
         }
 
-        private void OnValidatingRunLength( object sender, CancelEventArgs e )
+        private void OnValidatingRunLength( object sender, System.ComponentModel.CancelEventArgs e )
         {
             var control = sender as TextBox;
             var result = GetInt32(control);
             if (result < 0)
             {
-                _errors.SetError(control, "Must be < 0");
+                _errors.SetError(control, "Must be > 0");
                 e.Cancel = true;
             } else
                 _errors.SetError(control, "");
         }
+        #endregion
+
+        #region Private Members
+
+        private int GetInt32( TextBox textBox )
+        {
+            if (String.IsNullOrEmpty(textBox.Text))
+                return -1;
+
+            if (Int32.TryParse(textBox.Text, out var value))
+                return value;
+
+            return -1;
+        }
+        #endregion        
     }
 }
--- ClassWork/Section2/ITSE1430.MovieLib/MovieDatabase.cs
+++ ClassWork/Section2/ITSE1430.MovieLib/MovieDatabase.cs
@@ -6,107 +6,63 @@ using System.Threading.Tasks;
 
 namespace ITSE1430.MovieLib
 {
-    public class MovieDatabase
+    /// <summary>Manages a set of movies.</summary>
+    public abstract class MovieDatabase
     {
-        public MovieDatabase() : this(true)
-        {
-        }
-
-        private static Movie[] GetSeedMovies( bool seed )
-        {
-            if (!seed)
-                return new Movie[0];
-
-         return new [] {
-          new Movie()
-            {
-                Name = "Jaws",
-                RunLength = 120,
-                ReleaseYear = 1977
-            },
-           new Movie()
-            {
-                Name = "What About Bob?",
-                RunLength = 96,
-                ReleaseYear = 2004,
-            },
-          };
-        }
-        public MovieDatabase( bool seed ) : this(GetSeedMovies(seed))
-        {
-            //if (seed)
-            //{
-            //    var movie = new Movie();
-            //    movie.Name = "Jaws";
-            //    movie.RunLength = 120;
-            //    movie.ReleaseYear = 1977;
-            //    Add(movie);
-
-            //    movie = new Movie();
-            //    movie.Name = "What About Bob?";
-            //    movie.RunLength = 96;
-            //    movie.ReleaseYear = 2004;
-            //    Add(movie);
-            //};
-        }
-
-        public MovieDatabase( Movie[] movies )
-        {
-            _items.AddRange(movies);
-            //for (var index = 0; index < movies.Length; ++index)
-            //    _items[index] = movies[index];
-        }
+        /// <summary>Adds a movie to the database.</summary>
+        /// <param name="movie">The movie to add.</param>
         public void Add( Movie movie )
         {
-            _items.Add(movie);
-            //var index = FindNextFreeindex();
-            //if (index >= 0)
-            //    _movies[index] = movie;
+            //TODO: Validate
+            if (movie == null)
+                return;
+
+            AddCore(movie);
         }
 
-        //private Movie[] _movies = new Movie[100];
-        private List<Movie> _items = new List<Movie>();
+        protected abstract void AddCore( Movie movie );
 
+        /// <summary>Gets all the movies.</summary>
+        /// <returns>The list of movies.</returns>
         public Movie[] GetAll()
         {
-            //How many movies do we have
-            var count = _items.Count;
-
-            var temp = new Movie[count];
-            var index = 0;
-            foreach (var movie in _items)
-            {
-                temp[index++] = movie;
-            };
-            return temp;
+            return GetAllCore();
         }
 
+        protected abstract Movie[] GetAllCore();
+
+        /// <summary>Edits an existing movie.</summary>
+        /// <param name="name">The movie to edit.</param>
+        /// <param name="movie">The new movie.</param>
         public void Edit( string name, Movie movie )
         {
-            //Find movie by name
-            Remove(name);
+            //TODO: Validate
+            if (String.IsNullOrEmpty(name))
+                return;
+            if (movie == null)
+                return;
 
-            //Replace it
-            Add(movie);
+            //Find movie by name
+            var existing = FindByName(name);
+            if (existing == null)
+                return;
+
+            EditCore(existing, movie);
         }
 
+        protected abstract Movie FindByName( string name );
+        protected abstract void EditCore( Movie oldMovie, Movie newMovie );
+
+        /// <summary>Removes a movie.</summary>
+        /// <param name="name">The movie to remove.</param>
         public void Remove( string name )
         {
-            var movie = FindMovie(name);
-            if (movie != null)
-                _items.Remove(movie);
-        }
+            //TODO: Validate
+            if (String.IsNullOrEmpty(name))
+                return;
 
-        private Movie FindMovie( string name )
-        {
-            //for (var index = 0; index < _movies.Length; ++index)
-            foreach (var movie in _items)
-            {
-                // if (String.Compare(name, _movies[index]?.Name, true) == 0)
-                if (String.Compare(name, movie.Name, true) == 0)
-                    return movie;
-            };
-            return null;
+            RemoveCore(name);
         }
+        protected abstract void RemoveCore( string name );
     }
 }
