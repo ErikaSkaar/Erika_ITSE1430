c044c656a6051d33970b03c4e6f828259c63f179
ErikaSkaar <Erika.ballering@my.tccd.edu>
2018-11-04 22:47:39 -0600

    contact manager v.2

--- Labs/ContactManager.UI/ContactManager.UI/Form1.Designer.cs
+++ Labs/ContactManager.UI/ContactManager.UI/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 398);
+            this.ClientSize = new System.Drawing.Size(318, 455);
             this.Name = "Form1";
             this.Text = "Contacts Manager";
             this.ResumeLayout(false);
--- Labs/ContactManager.UI/ContactManager/Properties/AssemblyInfo.cs
+++ Labs/ContactManager.UI/ContactManager/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ContactManager.UI")]
+[assembly: AssemblyTitle("ContactManager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ContactManager.UI")]
+[assembly: AssemblyProduct("ContactManager")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0397ceb2-2115-41e5-9537-086b141930db")]
+[assembly: Guid("367061a5-ff15-49ef-ad44-01b5970fca5d")]
 
 // Version information for an assembly consists of the following four values:
 //
