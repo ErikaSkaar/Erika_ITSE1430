e66513cb4361eaa3c6ae581a731f91ca9da1d3fe
ErikaSkaar <Erika.ballering@my.tccd.edu>
2018-11-26 16:33:13 -0600

    added sqlProductDatabase

---
+++ Labs/startercode/startercode/Nile.Stores.Sql/SqlProductDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Stores.Sql
+{
+    public class SqlProductDatabase : ProductDatabase
+    {
+        protected override Product AddCore(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<Product> GetAllCore()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Product GetCore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RemoveCore(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Product UpdateCore(Product existing, Product newItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
